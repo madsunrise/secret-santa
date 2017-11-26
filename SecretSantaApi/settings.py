9692f4e2b93b6d734397cfa892484422505c134b
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-27 01:57:32 +0300

    Connected to MySQL and made migrations

--- SecretSantaApi/settings.py
+++ SecretSantaApi/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'SecretSantaApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'secret_santa',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
