9786f803634a4574eb7a8047a7a24b86437a7c3c
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-27 01:38:35 +0300

    Described models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+class SantaUser(models.Model):
+    user = models.OneToOneField(User)
+    wish = models.TextField()
+
+
+class Session(models.Model):
+    users = models.ManyToManyField(SantaUser)
+    date = models.DateTimeField(default=datetime.datetime.now)
+    key = models.CharField(max_length=6)
