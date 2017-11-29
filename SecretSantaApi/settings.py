0bc659b5f41dd026681159f7faf97084f07aaf5b
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-30 01:17:21 +0300

    Sending email

--- SecretSantaApi/settings.py
+++ SecretSantaApi/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '2017.secret.santa.2018@gmail.com'
+EMAIL_HOST_PASSWORD = '9162316828'
+EMAIL_USE_TLS = True
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
 
 from serializers import UserSerializer, SessionSerializer
 from models import SantaUser, Session
@@ -79,12 +79,52 @@ class SessionViewSet(viewsets.ModelViewSet):
 
         serializer = SessionSerializer(session)
         headers = self.get_success_headers(serializer.data)
+        sendConfirmationEmail(newUser.user.email)
         return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
 
 
 def randomString(length):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
+
 def randomId(length):
     return ''.join(random.choice(string.digits) for _ in range(length))
 
+
+def sendConfirmationEmail(email):
+    send_mail(
+        'Тайный Санта',
+        'Ура! Ты был зарегистрирован в Тайном Санте 2018! Держи случайный анекдот: \n' + randomJoke() + '\n\nС уважением, ваш Санта',
+        '2017.secret.santa.2018@gmail.com',
+        [email],
+        fail_silently=False,
+    )
+
+
+def randomJoke():
+    jokes = [
+        '''Разговор двух блондинок.
+— Представляешь! Говорят, что этот Новый год выпадет на пятницу!
+— Да-а! Только бы не на тринадцатое!''',
+        '''Приходит Дед Мороз к психиатру и говорит "Помогите доктор, я в себя не верю"!''',
+        '''В этом году письмо буду писать Снегурочке... )))) Она как женщина должна меня понять...!''',
+        '''Три возраста мужчины:
+- Он надеется, что его желания исполнит Дед Мороз.
+- Он надеется, что его желания исполнит Снегурочка.
+- Он надеется, что его желания исполнит Дед Мороз, если придет Снегурочка.''',
+        '''- Мама, мама! Ёлка горит!
+- Сынок, не горит, а сияет.
+- Мама, мама! Шторы сияют!''',
+        '''Дорогой Дедушка Мороз...
+         Я была хорошей девочкой весь год... Хм.. . Ну почти весь год... Хм.. . Ну иногда...
+          Хм.. . Ну пару раз то точно была... ОЙ ДА ЛАДНО, КУПЛЮ ВСЕ САМА!''',
+        '''На Новый год буду аналитиком! Буду следить: а у всех ли налито?''',
+        '''- А ты кем нарядишься на Новый год?
+- Кем, кем - сугробом! Снежинка из меня никакая!''',
+        '''Путём простейшей перестановки букв из Снегурочки может получиться как эпическая Огнесручка, так и абсолютно неполиткорректная Негросучка.
+А пожелание С Новым Годом превращается в брутальное Говно с дымом!''',
+        '''После празднования Нового года встречаются два приятеля:
+- Ну, как встретил праздник?
+- Да не знаю, еще не рассказывали...''',
+    ]
+    return random.choice(jokes)
