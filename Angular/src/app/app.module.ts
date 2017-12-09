6fdbe1814b3f0b21f868f593f5ccc8cd65a97f9d
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-12-09 19:38:35 +0300

    May be exports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
     imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, HttpModule, MatProgressBarModule ],
+    exports: [ MatProgressBarModule ],
     declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
